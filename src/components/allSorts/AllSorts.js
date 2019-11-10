104b35a7688a76035b74825ae01515f134ce39e9
ArvinLleva <arvin.lleva@yahoo.com>
2019-11-09 21:46:04 -0800

    initial commit

---
+++ src/components/allSorts/AllSorts.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class AllSorts extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            array: []
+        }
+    }
+    render() {
+        return (
+            <p>AllSorts</p>
+        )
+    }
+}
+
+export default AllSorts
